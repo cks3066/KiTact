afdc51f46e20f1b6a796516f534d17d35099f078
xeon1 <xeon1217@gmail.com>
2021-08-13 20:29:59 +0900

    Initial commit

---
+++ src/test/java/com/example/kitact/KiTactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiTactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
