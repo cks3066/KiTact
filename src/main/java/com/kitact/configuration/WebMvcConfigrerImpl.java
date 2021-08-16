50300674c46984b41f9539b5ce43dd1a525d2354
xeon1 <xeon1217@gmail.com>
2021-08-16 23:14:45 +0900

    CORS 대응 (http://localhost:3000)

--- src/main/java/com/kitact/configuration/WebMvcConfigrerImpl.java
+++ src/main/java/com/kitact/configuration/WebMvcConfigrerImpl.java
@@ -1,9 +1,11 @@
 package com.kitact.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class webMvcConfigrerImpl implements WebMvcConfigurer {
+@Configuration
+class WebMvcConfigurerImpl implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:3000");
