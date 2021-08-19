dc74e5b746500af7a1ea0308fe75ac70eb534021
xeon1 <xeon1217@gmail.com>
2021-08-20 03:51:37 +0900

    특정 요청에 대응하여 응답할 수 있는 service 구현

---
+++ src/main/java/com/kitact/service/ResponseService.java
@@ -0,0 +1,53 @@
+package com.kitact.service;
+
+import com.kitact.data.response.MultiResponse;
+import com.kitact.data.response.SingleResponse;
+import com.kitact.data.response.BaseResponse;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResponseService {
+    @Getter
+    public enum Response {
+        SUCCESS(0, "성공"),
+        FAIL(-1, "실패"),
+        ;
+
+        private final int code;
+        private final String message;
+
+        Response(int code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+    }
+    // 단순 성공 여부만 반환
+    public BaseResponse getSuccessResponse() {
+        BaseResponse response = new BaseResponse();
+        response.setSuccess(true);
+        response.setCode(Response.SUCCESS.getCode());
+        response.setMessage(Response.SUCCESS.getMessage());
+        return response;
+    }
+
+    // 단순 실패 여부만 반환
+    public BaseResponse getFailResponse() {
+        BaseResponse response = new BaseResponse();
+        response.setSuccess(false);
+        response.setCode(Response.FAIL.getCode());
+        response.setMessage(Response.FAIL.getMessage());
+        return response;
+    }
+
+    // 실패 여부만 반환 (매개변수를 통해 정의된 오류 반환)
+    public BaseResponse getFailResponse(Response responseType) {
+        BaseResponse response = new BaseResponse();
+        response.setSuccess(false);
+        response.setCode(responseType.getCode());
+        response.setMessage(responseType.getMessage());
+        return response;
+    }
+}
