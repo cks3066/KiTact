871901bbcc6360330d479b28ee495fbbbc17c4ed
xeon1 <xeon1217@gmail.com>
2021-08-20 04:47:42 +0900

    Response 객체 구현에 따른 기능 대응

--- src/main/java/com/kitact/controller/UserController.java
+++ src/main/java/com/kitact/controller/UserController.java
@@ -4,7 +4,10 @@ import com.kitact.configuration.security.JWTProvider;
 import com.kitact.data.dto.SignInRequestDTO;
 import com.kitact.data.dto.SignUpRequestDTO;
 import com.kitact.data.model.User;
+import com.kitact.data.response.BaseResponse;
+import com.kitact.data.response.SingleResponse;
 import com.kitact.repository.UserRepository;
+import com.kitact.service.ResponseService;
 import com.kitact.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +20,17 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserRepository userRepository;
     private final UserService userService;
+    private final ResponseService responseService;
 
     @PostMapping("/sign-in")
-    public String signIn(@RequestBody SignInRequestDTO signInRequestDTO) {
-        return userService.signInUser(signInRequestDTO);
+    public BaseResponse signIn(@RequestBody SignInRequestDTO signInRequestDTO) {
+        return responseService.getSingleResponse(userService.signInUser(signInRequestDTO));
     }
 
     // 회원가입 로직
     @PostMapping("/sign-up")
-    public String signUp(@RequestBody SignUpRequestDTO signUpRequestDTO) {
+    public BaseResponse signUp(@RequestBody SignUpRequestDTO signUpRequestDTO) {
         userService.signUpUser(signUpRequestDTO);
-        return "redirect:/";
+        return responseService.getSuccessResponse();
     }
 }
