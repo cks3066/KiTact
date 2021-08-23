bf990a86b70780998c573ec9ddec40adef1825d5
On <cks3066@naver.com>
2021-08-24 00:24:49 +0900

    no message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,25 @@
-<<<<<<< HEAD
 import React from 'react'
-import { BrowserRouter, Router, withRouter, Route } from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
-=======
-import React from "react";
-import { BrowserRouter as ConnectedRouter, Route } from "react-router-dom";
->>>>>>> f8fc3fe58beebecc9c1b083b9e956716429c1795
-
+import { Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-
-<<<<<<< HEAD
 import Search from './pages/Search'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 
 import { Restaurant } from './component/Restaurant'
 import Connect from './Connect'
-import { connect } from 'react-redux'
-import { loadMenu, createMenu, calculate, increment, decrement } from './redux/modules/menu'
-
 import { apiKey } from './shared/Firebase'
-
 import { actionCreators as userActions } from './redux/modules/user'
 import { useDispatch } from 'react-redux'
 
-import { history } from './redux/configStore'
+import { history } from "./redux/configStore";
+import { Order } from "./pages/Order";
+
 import Header from './component/Header'
 
-const mapStateToProps = state => {
-  return { menu_list: state.menu.list }
-}
-
-const mapDispatchToPOrops = dispatch => {
-  return {
-    load: () => {
-      dispatch(loadMenu())
-    },
-    create: menu => {
-      dispatch(createMenu(menu))
-    },
-    calculate: menu => {
-      dispatch(calculate(menu))
-    },
-    increment: index => {
-      dispatch(increment(index))
-    },
-    decrement: index => {
-      dispatch(decrement(index))
-    },
-  }
-}
 
 function Copyright() {
   return (
@@ -63,18 +30,6 @@ function Copyright() {
   )
 }
 
-=======
-import { DashboardComponent } from "./DashboardComponent";
-import Search from "./pages/Search";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-
-import { TopMenuComponent } from "./component/TopMenuComponent";
-import Connect from "./Connect";
-import { history } from "./redux/configStore";
-import { Order } from "./pages/Order";
-
->>>>>>> f8fc3fe58beebecc9c1b083b9e956716429c1795
 function App() {
   const dispatch = useDispatch()
 
@@ -85,45 +40,30 @@ function App() {
     if (is_session) {
       dispatch(userActions.loginCheckFB())
     }
-  }, [])
-  ;<Connect />
+  }, []);
+
+  <Connect />
   return (
     <div className='App'>
       <div>
-<<<<<<< HEAD
         {/* Drawer & Header */}
         <Header />
-
         <ConnectedRouter history={history}>
           <Route path='/' exact component={Search} />
           <Route path='/menulist' exact component={Restaurant} />
           <Route path='/map' exact component={Search} />
           <Route path='/login' exact component={Login} />
           <Route path='/signup' exact component={Signup} />
+          <Route path="/order" component={Order} />
         </ConnectedRouter>
 
         {/* Footer */}
         <Box mt={5}>
           <Copyright />
         </Box>
-=======
-        <TopMenuComponent />
-        <ConnectedRouter history={history}>
-          <Route path="/" exact component={Search} />
-          <Route path="/order" component={Order} />
-          <Route path="/dashboard" component={DashboardComponent} />
-          <Route path="/map" component={Search} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-        </ConnectedRouter>
->>>>>>> f8fc3fe58beebecc9c1b083b9e956716429c1795
       </div>
     </div>
   )
 }
 
-<<<<<<< HEAD
-export default connect(mapStateToProps, mapDispatchToPOrops)(withRouter(App))
-=======
 export default App;
->>>>>>> f8fc3fe58beebecc9c1b083b9e956716429c1795
--- frontend/src/redux/configStore.js
+++ frontend/src/redux/configStore.js
@@ -1,75 +1,45 @@
-<<<<<<< HEAD
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { createBrowserHistory } from "history";
-import { connectRouter } from "connected-react-router";
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { createBrowserHistory } from 'history'
+import { connectRouter } from 'connected-react-router'
 
-import menu from "./modules/menu";
-import User from "./modules/user";
-=======
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
-import { createBrowserHistory } from "history";
-import thunk from "redux-thunk";
-import { connectRouter } from "connected-react-router";
->>>>>>> f8fc3fe58beebecc9c1b083b9e956716429c1795
+import User from './modules/user'
 
 // 리덕스 모듈 등록
-import Restaurant from "./modules/restaurant";
+import Restaurant from './modules/restaurant'
 
 // 컴포넌트 이동 경로: ex) 뒤로가기
-export const history = createBrowserHistory();
+export const history = createBrowserHistory()
 
-<<<<<<< HEAD
-const rootReducer = combineReducers({
-    menu,
-    user: User,
-    router: connectRouter(history),
-  });
-
-const middlewares = [thunk.withExtraArgument({ history: history })];
-
-const env = process.env.NODE_ENV;
-
-=======
 // reducer에서 history 사용
 const rootReducer = combineReducers({
   restaurant: Restaurant,
+  user: User,
   router: connectRouter(history),
-});
+})
 
 // axios → then → history 절차적 경로 제공
 // 새로운 미들웨어 추가는 여기서
-const middlewares = [thunk.withExtraArgument({ history: history })];
+const middlewares = [thunk.withExtraArgument({ history: history })]
 
 // 현재 환경 정의
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV
 
 // 개발환경 한정으로 로거를 가져와서 사용
->>>>>>> f8fc3fe58beebecc9c1b083b9e956716429c1795
-if (env === "development") {
-  const { logger } = require("redux-logger");
-  middlewares.push(logger);
+if (env === 'development') {
+  const { logger } = require('redux-logger')
+  middlewares.push(logger)
 }
 
-<<<<<<< HEAD
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
-    : compose;
-
-=======
 // 리덕스 개발자 도구 설정
 const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  : compose;
 
 // 미들웨어에 리덕스 개발자 도구 적용
->>>>>>> f8fc3fe58beebecc9c1b083b9e956716429c1795
-const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+const enhancer = composeEnhancers(applyMiddleware(...middlewares))
 
-let store = (initialStore) => createStore(rootReducer, enhancer);
+let store = initialStore => createStore(rootReducer, enhancer)
 
-export default store();
+export default store()
