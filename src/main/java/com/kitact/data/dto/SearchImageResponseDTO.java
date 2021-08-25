b5b1cb4b1f568a5e53cfd836e1867d54eb46c10d
xeon1 <xeon1217@gmail.com>
2021-08-26 03:27:25 +0900

    네이버 검색 기능 관련 DTO 정리

--- src/main/java/com/kitact/data/dto/SearchImageResponseDTO.java
+++ src/main/java/com/kitact/data/dto/SearchImageResponseDTO.java
@@ -6,15 +6,16 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+// 지역 검색 출력 결과를 변수화
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchImageResponseDTO {
-    private String lastBuildDate;
-    private int total;
-    private int start;
-    private int display;
-    private List<SearchImageItem> items;
+    private String lastBuildDate; // 검색 결과를 생성한 시간이다.
+    private int total; //  검색 결과 문서의 총 개수를 의미한다.
+    private int start; // 검색 결과 문서 중, 문서의 시작점을 의미한다.
+    private int display; // 검색된 검색 결과의 개수이다.
+    private List<SearchImageItem> items; // XML 포멧에서는 item 태그로, JSON 포멧에서는 items 속성으로 표현된다. 개별 검색 결과이며 title, link, description, address, mapx, mapy를 포함한다.
 
     @Data
     @NoArgsConstructor
--- src/main/java/com/kitact/data/dto/SearchLocalRequestDTO.java
+++ src/main/java/com/kitact/data/dto/SearchLocalRequestDTO.java
@@ -4,14 +4,15 @@ import lombok.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+// 지역 검색 요청 변수에 대한 변수 생성
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchLocalRequestDTO {
-    private String query = "";
-    private int display = 1;
-    private int start = 1;
-    private String sort = Sort.RANDOM.getStr();
+    private String query = ""; // 검색을 원하는 문자열로서 UTF-8로 인코딩한다.
+    private int display = 1; // 검색 결과 출력 건수 지정(1 ~ 5)
+    private int start = 1; // 검색 시작 위치로 1만 가능
+    private String sort = Sort.RANDOM.getStr(); // 정렬 옵션: random(유사도순), comment(카페/블로그 리뷰 개수 순)
 
     public MultiValueMap<String, String> toMultiValueMap() {
         LinkedMultiValueMap<String, String> map = new LinkedMultiValueMap<>();
