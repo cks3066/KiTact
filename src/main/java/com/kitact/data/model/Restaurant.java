50adf01ae4f8fb4c3063ad876bce906492b67be9
xeon1 <xeon1217@gmail.com>
2021-08-26 03:15:28 +0900

    Restaurant Entity 변경

--- src/main/java/com/kitact/data/model/Restaurant.java
+++ src/main/java/com/kitact/data/model/Restaurant.java
@@ -27,50 +27,70 @@ public class Restaurant extends Timestamped {
     @JoinColumn
     private Wishlist wishlist_id;
 
-    @Column(nullable = false, length = 50)
+    @Column(length = 10)
+    private String large_category;
+
+    @Column(length = 10)
+    private String medium_category;
+
+    @Column(length = 10)
+    private String small_category;
+
+    @Column(length = 50)
     private String restaurant_name;
 
-    @Column(length = 100)
-    private String tag;
+    @Column
+    private String img;
 
     @Column(length = 100)
     private String address;
 
     @Column(length = 15)
-    private String telephone;
-
-    @Column
-    private String restaurant_image;
+    private String tel;
 
     @Column(length = 30)
-    private String openinghours;
+    private String time;
 
-    @Column(nullable = false, length = 10)
-    private String big_category;
+    @Column(length = 200)
+    private String detail;
 
-    @Column(nullable = false, length = 10)
-    private String small_category;
-
-    @Column
-    private Double lat;
+    @Column(length = 200)
+    private String tags;
 
     @Column
-    private Double lng;
+    private Integer lat;
 
-    public Restaurant(String restaurant_name, String tag,
-                      String address, String telephone,
-                      String restaurant_image, String openinghours,
-                      String big_category, String small_category,
-                      Double lat, Double lng) {
-        this.restaurant_name = restaurant_name;
-        this.tag = tag;
-        this.address = address;
-        this.telephone = telephone;
-        this.restaurant_image = restaurant_image;
-        this.openinghours = openinghours;
-        this.big_category = big_category;
+    @Column
+    private Integer lng;
+
+    @Column
+    private Integer total_seat_count;
+
+    @Column
+    private Integer vacancy_count;
+
+
+    public Restaurant(String large_category, String medium_category, String small_category,
+                      String restaurant_name,
+                      String img,
+                      String address, String tel,
+                      String time,
+                      String detail, String tags,
+                      Integer lat, Integer lng,
+                      Integer total_seat_count, Integer vacancy_count) {
+        this.large_category = large_category;
+        this.medium_category = medium_category;
         this.small_category = small_category;
+        this.restaurant_name = restaurant_name;
+        this.img = img;
+        this.address = address;
+        this.tel = tel;
+        this.time = time;
+        this.detail = detail;
+        this.tags = tags;
         this.lat = lat;
         this.lng = lng;
+        this.total_seat_count = total_seat_count;
+        this.vacancy_count = vacancy_count;
     }
 }
