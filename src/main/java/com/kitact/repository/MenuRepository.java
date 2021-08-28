5b81c080ccb340ec9148c2f9ba2d85802f018f0e
xeon1 <xeon1217@gmail.com>
2021-08-28 09:13:16 +0900

    Menu 관련 CURD 구현

--- src/main/java/com/kitact/repository/MenuRepository.java
+++ src/main/java/com/kitact/repository/MenuRepository.java
@@ -1,7 +1,12 @@
 package com.kitact.repository;
 
 import com.kitact.data.model.Menu;
+import com.kitact.data.model.Restaurant;
+import com.kitact.data.model.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    List<Menu> findAllByRestaurant(Restaurant restaurant);
 }
